8d1750f870441eec819b836cba403633b0c4e2ea
Lienard Colin <colin.lienard@etu.unilim.fr>
2021-11-04 18:19:07 +0100

    :hammer: add server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import { Server } from 'socket.io';
+import { addUser, getUsers, removeUser } from './users.js';
+
+const io = new Server(8000, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ room, author, content }) => {
+    socket.to(room).emit('message', author, content);
+  });
+
+  socket.on('join', ({ userName, userId, room }) => {
+    socket.join(room);
+    socket.to(room).emit('user-joined', userName, userId);
+    addUser(userName, userId, socket.id, room);
+    io.to(socket.id).emit('get-users', getUsers(room));
+  });
+
+  // socket.on('leave', ({ userName, room }) => {
+  //   socket.leave(room);
+  //   io.to(room).emit('user-left', userName);
+  //   io.to(room).emit('get-users', getUsers(room));
+  // });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('user-left', user.userName);
+      io.to(user.room).emit('get-users', getUsers(user.room));
+    }
+  });
+});
---
+++ server/users.js
@@ -0,0 +1,27 @@
+let users = [];
+
+export const addUser = (userName, userId, socketId, room) => {
+  const user = {
+    userName,
+    userId,
+    socketId,
+    room,
+  };
+  users = [...users, user];
+};
+
+export const removeUser = (id) => {
+  let toRemove = null;
+  users = users.filter((user) => {
+    if (user.socketId === id) {
+      toRemove = user;
+      return false;
+    }
+    return true;
+  });
+  return toRemove;
+};
+
+export const getUsers = (room) => users.filter((user) => user.room === room);
+
+// export const findUser = (id) => users.filter((user) => user.id === id);
