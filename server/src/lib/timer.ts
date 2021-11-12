54171525889228a4d3cd23be8921308fc51ffcaa
Colin Lienard <colin.lienard87@orange.fr>
2021-11-12 18:40:03 +0100

    :bug: fix timer not working

--- server/src/lib/timer.ts
+++ server/src/lib/timer.ts
@@ -8,15 +8,17 @@ export const stopTimer = (room: string) => {
 };
 
 export const startTimer = (io: Server, room: string, start: number, callback: () => void) => {
-  let time = start;
-  io.to(room).emit('timer', time);
-  getRoom(room).timer = setInterval(() => {
-    if (time > 0) {
-      time -= 1;
-      io.to(room).emit('timer', time);
-    } else {
-      stopTimer(room);
-      callback();
-    }
-  }, 1000);
+  if (getRoom(room)) {
+    let time = start;
+    io.to(room).emit('timer', time);
+    getRoom(room).timer = setInterval(() => {
+      if (time > 0) {
+        time -= 1;
+        io.to(room).emit('timer', time);
+      } else {
+        stopTimer(room);
+        callback();
+      }
+    }, 1000);
+  }
 };
