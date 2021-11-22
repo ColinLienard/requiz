d98b87093b82410b8fabd4e3e6c8e2359aa9ec94
Colin Lienard <colin.lienard87@orange.fr>
2021-11-22 18:23:11 +0100

    :construction: create and modify a quiz

---
+++ website/pages/api/save-quiz.ts
@@ -0,0 +1,19 @@
+import { ObjectId } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '../../lib/utils/mongodb';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { quizId, title, description } = JSON.parse(req.body);
+  const client = await clientPromise;
+  const response = await client.db().collection('quizzes').updateOne({ _id: new ObjectId(quizId) }, {
+    $set: {
+      title,
+      description,
+    },
+  });
+  if (response.modifiedCount) {
+    res.status(200).end();
+  } else {
+    res.status(400).end();
+  }
+};
