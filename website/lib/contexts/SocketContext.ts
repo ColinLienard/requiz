3fa539131945e55f87746dfb9d760e6ebf8612e4
Colin Lienard <colin.lienard87@orange.fr>
2021-12-29 11:27:32 +0100

    :fire: Remove website folder

--- website/lib/contexts/SocketContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { io, Socket } from 'socket.io-client';
-
-const SocketContext = createContext<Socket>(io(process.env.NEXT_PUBLIC_SERVER_URL as string));
-
-export default SocketContext;
