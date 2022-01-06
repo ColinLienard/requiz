0fd650cf215a08b5bc30e252a699e9866610c64d
Colin Lienard <colin.lienard87@orange.fr>
2022-01-06 19:44:14 +0100

    :lipstick: Add profile popup

--- app/components/Common/Navbar/Navbar.tsx
+++ app/components/Common/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import { signIn } from 'next-auth/react';
 import Popup from 'react-customizable-popup';
 import useIntersection from '../../../lib/hooks/useIntersection';
 import { UserFromDB } from '../../../lib/types';
+import CrossIcon from '../../../public/icons/iconComponents/CrossIcon';
 import styles from './Navbar.module.scss';
 
 type Props = {
@@ -17,19 +18,35 @@ const Navbar: FC<Props> = ({ user }) => {
   const renderProfilePicture = () => {
     if (user?.image) {
       return (
-        // <Popup
-        //   toggler={(
-        //     <button className={styles.profilePictureButton} type="button">
-        //       <Image className={styles.profilePicture} src={user.image} width={28} height={28} />
-        //     </button>
-        //   )}
-        // >
-        //   {user.name}
-        //   {user.email}
-        // </Popup>
-        <button className={styles.profilePictureButton} type="button">
-          <Image className={styles.profilePicture} src={user.image} width={28} height={28} />
-        </button>
+        <Popup
+          root="#__next"
+          toggler={(
+            <button className={styles.profilePictureButton} type="button">
+              <Image className={styles.profilePicture} src={user.image} width={28} height={28} />
+            </button>
+          )}
+          className={styles.popup}
+          backdropClassName="b"
+          position={['midleft', 'bottom']}
+          fixed
+          distanceFromEdges={24}
+        >
+          <button className={styles.close} type="button" data-close>
+            <CrossIcon />
+          </button>
+          <section className={`${styles.section} ${styles.centered}`}>
+            <Image className={styles.profilePicture} src={user.image} width={64} height={64} />
+            <h2 className={styles.name}>{user.name}</h2>
+            <p className={styles.email}>{user.email}</p>
+            <button className={styles.button} type="button">See your profile</button>
+          </section>
+          <section className={styles.section}>
+            <h3 className={styles.title}>Online friends</h3>
+          </section>
+          <section className={styles.section}>
+            <h3 className={styles.title}>Language</h3>
+          </section>
+        </Popup>
       );
     }
     return (
