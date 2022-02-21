4092adf03eef4356b20f695a68738497a8654be4
Colin Lienard <colin.lienard87@orange.fr>
2022-02-21 19:03:28 +0100

    :sparkles: Copy url of your quiz to clipboard

--- app/components/Dashboard/Modal/Modal.tsx
+++ app/components/Dashboard/Modal/Modal.tsx
@@ -7,10 +7,13 @@ import {
 import Link from 'next/link';
 import { QuizData } from '../../../lib/types';
 import UserItem from '../../Common/UserItem/UserItem';
-import CrossIcon from '../../../public/icons/iconComponents/CrossIcon';
-import styles from './Modal.module.scss';
+import QuizStatusIndicator from '../../Common/QuizStatusIndicator/QuizStatusIndicator';
+import Alert, { AlertHandle } from '../../Common/Alert/Alert';
 import useDate from '../../../lib/hooks/useDate';
 import useQuizTheme from '../../../lib/hooks/useQuizTheme';
+import CrossIcon from '../../../public/icons/iconComponents/CrossIcon';
+import ShareIcon from '../../../public/icons/iconComponents/ShareIcon';
+import styles from './Modal.module.scss';
 
 type Props = {
   quiz?: QuizData,
@@ -21,7 +24,7 @@ const Modal: FC<Props> = ({ quiz, onClose }) => {
   const [visible, setVisible] = useState(false);
   const date = useDate(quiz?.startDate as string);
   const theme = useQuizTheme(quiz?.theme);
-  const quizUrl = useRef('');
+  const alert = useRef<AlertHandle>(null);
 
   const handleClose = () => {
     setVisible(false);
@@ -44,16 +47,23 @@ const Modal: FC<Props> = ({ quiz, onClose }) => {
 
   useEffect(() => {
     if (quiz) {
-      quizUrl.current = `${window.location.origin}/quiz/${quiz._id}`;
-      console.log(quizUrl);
       setVisible(true);
     } else {
       handleClose();
     }
   }, [quiz]);
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(`${window.location.origin}/quiz/${quiz?._id}`);
+    alert.current?.alert(
+      'The url of your quiz has been copied to your clipboard. Share it so people can play your quiz.',
+      'success',
+    );
+  };
+
   return (
     <>
+      <Alert ref={alert} />
       <div
         className={`${styles.backdrop} ${visible && styles.visible}`}
         onClick={handleClose}
@@ -63,9 +73,16 @@ const Modal: FC<Props> = ({ quiz, onClose }) => {
       <aside className={`${styles.modal} ${visible && styles.visible}`}>
         <div className={`${styles.header} ${visible && styles.visible}`} style={{ backgroundColor: theme?.color }}>
           <div className={styles.noise} />
-          <button className={styles.cross} type="button" onClick={handleClose}>
-            <CrossIcon />
-          </button>
+          <div className={styles.icons}>
+            <button className={styles.cross} type="button" onClick={handleClose}>
+              <CrossIcon />
+            </button>
+            {quiz?.status && (
+              <button className={styles.share} type="button" onClick={copyToClipboard}>
+                <ShareIcon />
+              </button>
+            )}
+          </div>
           <span className={styles.theme}>
             <span className={styles.emoji}>{theme?.emoji}</span>
             {theme?.name}
@@ -74,11 +91,13 @@ const Modal: FC<Props> = ({ quiz, onClose }) => {
         </div>
         <div className={styles.main}>
           <div className={styles.userContainer}>
-            <p>by</p>
             {quiz?.status ? (
-              <p>you</p>
+              <QuizStatusIndicator status={quiz?.status} />
             ) : (
-              <UserItem id={quiz?.userId} small />
+              <>
+                <p>by</p>
+                <UserItem id={quiz?.userId} small />
+              </>
             )}
           </div>
           <p className={styles.description}>{quiz?.description}</p>
